c5809d878d8b7c8e2118f4ecc335d5ded3d59856
hugo scheithauer <hugoscheithauer@outlook.fr>
2021-04-11 15:53:23 +0200

    réorganisation repo

--- run.py
+++
@@ -1,7 +0,0 @@
-from app.app import config_app
-
-if __name__ == "__main__":
-    # On indique la configuration de l'app choisie
-    app = config_app("test")
-    # On lance l'app en mode debug, au cas où cela serait nécessaire
-    app.run(debug=True)
