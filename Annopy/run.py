06cfc709e0dc85cac51dabe42a3ae0e81ec29d0e
hugo scheithauer <hugoscheithauer@outlook.fr>
2021-04-11 23:26:47 +0200

    modification nom config

--- Annopy/run.py
+++ Annopy/run.py
@@ -2,6 +2,6 @@ from app.app import config_app
 
 if __name__ == "__main__":
     # On indique la configuration de l'app choisie
-    app = config_app("test")
+    app = config_app("dev")
     # On lance l'app en mode debug, au cas où cela serait nécessaire
     app.run(debug=True)
